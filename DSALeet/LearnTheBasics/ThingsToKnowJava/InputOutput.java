f8204a524b770a2d0cc73e1d2576b2c1e94963e5
BS <bsbalaji101005@gmail.com>
2026-03-27 19:11:10 +0530

    Package building & Input & Output

---
+++ DSALeet/LearnTheBasics/ThingsToKnowJava/InputOutput.java
@@ -0,0 +1,16 @@
+package ThingsToKnowJava;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input number: ");
+		int num = sc.nextInt();
+		System.out.println("Output: "+num);
+	}
+
+}
